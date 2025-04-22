7cbb8a91d8f6c4bebdf6d1b68337727b763644d1
gretaleege <greta.k.leege@vanderbilt.edu>
2025-04-22 19:17:09 +0200

    mysterious name account manager

--- G8X.2025.T00.GE4-main/src/main/python/uc3m_money/transfer_request.py
+++ G8X.2025.T00.GE4-main/src/main/python/uc3m_money/transfer_request.py
@@ -3,16 +3,18 @@ import hashlib
 import json
 from datetime import datetime, timezone
 
+
 class TransferRequest:
     """Class representing a transfer request"""
-    #pylint: disable=too-many-arguments
+
+    # pylint: disable=too-many-arguments
     def __init__(self,
                  from_iban: str,
                  transfer_type: str,
-                 to_iban:str,
-                 transfer_concept:str,
-                 transfer_date:str,
-                 transfer_amount:float):
+                 to_iban: str,
+                 transfer_concept: str,
+                 transfer_date: str,
+                 transfer_amount: float):
         self.__from_iban = from_iban
         self.__to_iban = to_iban
         self.__transfer_type = transfer_type
@@ -43,7 +45,7 @@ class TransferRequest:
         }
 
     @property
-    def from_iban(self) :
+    def from_iban(self):
         """Sender's IBAN."""
         return self.__from_iban
 
@@ -52,7 +54,7 @@ class TransferRequest:
         self.__from_iban = value
 
     @property
-    def to_iban(self) :
+    def to_iban(self):
         """receiver's iban"""
         return self.__to_iban
 
@@ -61,17 +63,19 @@ class TransferRequest:
         self.__to_iban = value
 
     @property
-    def transfer_type(self) :
+    def transfer_type(self):
         """Property representing the type of transfer: REGULAR, INMEDIATE or URGENT """
         return self.__transfer_type
+
     @transfer_type.setter
     def transfer_type(self, value):
         self.__transfer_type = value
 
     @property
-    def transfer_amount(self) :
+    def transfer_amount(self):
         """Property respresenting the transfer amount"""
         return self.__transfer_amount
+
     @transfer_amount.setter
     def transfer_amount(self, value):
         self.__transfer_amount = value
@@ -80,21 +84,23 @@ class TransferRequest:
     def transfer_concept(self):
         """Property representing the transfer concept"""
         return self.__concept
+
     @transfer_concept.setter
     def transfer_concept(self, value):
         self.__concept = value
 
     @property
-    def transfer_date( self ) :
+    def transfer_date(self):
         """Property representing the transfer's date in DD/MM/YYYY format"""
         return self.__transfer_date
+
     @transfer_date.setter
-    def transfer_date( self, value ):
+    def transfer_date(self, value):
         self.__transfer_date = value
 
     @property
-    def time_stamp(self) :
-        """Read only property that returns the timestamp of the request"""
+    def time_stamp(self):
+        """Read-only property that returns the timestamp of the request"""
         return self.__time_stamp
 
     @property
