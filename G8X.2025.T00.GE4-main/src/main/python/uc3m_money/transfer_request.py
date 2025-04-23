3131af03261100331ed5b3e467888feb2948371e
gretaleege <greta.k.leege@vanderbilt.edu>
2025-04-23 20:28:26 +0200

    transfer request update

--- G8X.2025.T00.GE4-main/src/main/python/uc3m_money/transfer_request.py
+++ G8X.2025.T00.GE4-main/src/main/python/uc3m_money/transfer_request.py
@@ -3,16 +3,18 @@ import hashlib
 import json
 from datetime import datetime, timezone
 
+
 class TransferRequest:
     """Class representing a transfer request"""
-    #pylint: disable=too-many-arguments
+
+    # pylint: disable=too-many-arguments
     def __init__(self,
                  from_iban: str,
                  transfer_type: str,
-                 to_iban:str,
-                 transfer_concept:str,
-                 transfer_date:str,
-                 transfer_amount:float):
+                 to_iban: str,
+                 transfer_concept: str,
+                 transfer_date: str,
+                 transfer_amount: float):
         self.__from_iban = from_iban
         self.__to_iban = to_iban
         self.__transfer_type = transfer_type
@@ -22,8 +24,12 @@ class TransferRequest:
         justnow = datetime.now(timezone.utc)
         self.__time_stamp = datetime.timestamp(justnow)
 
-    def __str__(self):
-        return "Transfer:" + json.dumps(self.__dict__)
+    def signature_string(self) -> str:
+        """
+        Composes a string representation of the transfer data
+        to be hashed and used as the unique transfer code.
+        """
+        return f"Transfer:{json.dumps(self.__dict__)}"
 
     def to_json(self):
         """returns the object information in json format"""
@@ -37,9 +43,10 @@ class TransferRequest:
             "time_stamp": self.__time_stamp,
             "transfer_code": self.transfer_code
         }
+
     @property
     def from_iban(self):
-        """Sender's iban"""
+        """Sender's IBAN."""
         return self.__from_iban
 
     @from_iban.setter
@@ -59,6 +66,7 @@ class TransferRequest:
     def transfer_type(self):
         """Property representing the type of transfer: REGULAR, INMEDIATE or URGENT """
         return self.__transfer_type
+
     @transfer_type.setter
     def transfer_type(self, value):
         self.__transfer_type = value
@@ -67,6 +75,7 @@ class TransferRequest:
     def transfer_amount(self):
         """Property respresenting the transfer amount"""
         return self.__transfer_amount
+
     @transfer_amount.setter
     def transfer_amount(self, value):
         self.__transfer_amount = value
@@ -75,21 +84,23 @@ class TransferRequest:
     def transfer_concept(self):
         """Property representing the transfer concept"""
         return self.__concept
+
     @transfer_concept.setter
     def transfer_concept(self, value):
         self.__concept = value
 
     @property
-    def transfer_date( self ):
-        """Property representing the transfer's date"""
+    def transfer_date(self):
+        """Property representing the transfer's date in DD/MM/YYYY format"""
         return self.__transfer_date
+
     @transfer_date.setter
-    def transfer_date( self, value ):
+    def transfer_date(self, value):
         self.__transfer_date = value
 
     @property
     def time_stamp(self):
-        """Read-only property that returns the timestamp of the request"""
+        """Read only property that returns the timestamp of the request"""
         return self.__time_stamp
 
     @property
