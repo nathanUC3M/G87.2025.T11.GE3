68590ff3f8a905ac76a7cf1e0da8ca709fbd2e84
gretaleege <greta.k.leege@vanderbilt.edu>
2025-04-25 21:26:03 +0200

    quick pylint

--- src/main/python/uc3m_money/iban_balance.py
+++ src/main/python/uc3m_money/iban_balance.py
@@ -1,16 +1,36 @@
+"""
+Provides the IbanBalance class to calculate and serialize the balance for a given IBAN.
+Handles reading transactions from a JSON file and raises AccountManagementException on errors.
+"""
+import json
+from datetime import datetime, timezone
 from uc3m_money.account_management_exception import AccountManagementException
 from uc3m_money.data.attr.iban_code import IbanCode
 from uc3m_money.account_management_config import TRANSACTIONS_STORE_FILE
-from datetime import datetime, timezone
-import json
 
-class IbanBalance():
+
+class IbanBalance:
+    """
+    Represents the balance of an account identified by an IBAN.
+
+    This class provides methods to calculate the balance for a specific IBAN
+    by reading the transactions from a JSON file and serializing the result.
+    """
     def __init__(self, iban):
+        """
+        Initializes the IbanBalance object, validates the IBAN,
+        sets the timestamp for the balance, and calculates the balance
+        """
         self._iban = IbanCode(iban).value
         self.__last_balance_time = datetime.timestamp(datetime.now(timezone.utc))
         self.__balance = self.calculate_iban_balance()
 
     def calculate_iban_balance(self):
+        """
+        Calculates the current balance for the IBAN by summing up all transactions
+        associated with it.
+        """
+
         transactions_list = self.read_transactions_file()
         iban_found = False
         current_balance = 0
@@ -23,7 +43,8 @@ class IbanBalance():
             raise AccountManagementException("IBAN not found")
         return current_balance
 
-    def read_transactions_file(self):
+    @staticmethod
+    def read_transactions_file():
         """loads the content of the transactions file
         and returns a list"""
         try:
@@ -36,6 +57,9 @@ class IbanBalance():
         return input_list
 
     def to_json(self):
+        """
+        Loads and parses the transactions JSON file.
+        """
         return {"IBAN": self._iban,
                         "time": self.__last_balance_time,
                         "BALANCE": self.__balance}
