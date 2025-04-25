96eceeb0976947f7a5fd2dbab293ef5bd2f848e6
Nathan Hammond <nathan.hammond@vanderbilt.edu>
2025-04-25 12:45:47 -0500

    attribute video push

---
+++ src/main/python/uc3m_money/data/attr/iban_code.py
@@ -0,0 +1,47 @@
+from uc3m_money.account_management_exception import AccountManagementException
+from uc3m_money.data.attr.attribute import Attribute
+
+class IbanCode(Attribute):
+    def __init__(self, attr_value):
+        self._validation_pattern = r"^ES[0-9]{22}"
+        self._error_message = "Invalid IBAN format"
+        self._attr_value = self._validate(attr_value)
+
+    def _validate(self, attr_value):
+        attr_value = super()._validate(attr_value)
+        iban = attr_value
+        original_code = iban[2:4]
+        # replacing the control
+        iban = iban[:2] + "00" + iban[4:]
+        iban = iban[4:] + iban[:4]
+
+        # Convertir el IBAN en una cadena numérica, reemplazando letras por números
+        iban = (iban.replace('A', '10').replace('B', '11').
+                replace('C', '12').replace('D', '13').replace('E', '14').
+                replace('F', '15'))
+        iban = (iban.replace('G', '16').replace('H', '17').
+                replace('I', '18').replace('J', '19').replace('K', '20').
+                replace('L', '21'))
+        iban = (iban.replace('M', '22').replace('N', '23').
+                replace('O', '24').replace('P', '25').replace('Q', '26').
+                replace('R', '27'))
+        iban = (iban.replace('S', '28').replace('T', '29').replace('U', '30').
+                replace('V', '31').replace('W', '32').replace('X', '33'))
+        iban = iban.replace('Y', '34').replace('Z', '35')
+
+        # Mover los cuatro primeros caracteres al final
+
+        # Convertir la cadena en un número entero
+        int_i = int(iban)
+
+        # Calcular el módulo 97
+        mod = int_i % 97
+
+        # Calcular el dígito de control (97 menos el módulo)
+        dc = 98 - mod
+
+        if int(original_code) != dc:
+            # print(dc)
+            raise AccountManagementException("Invalid IBAN control digit")
+
+        return attr_value
--- src/main/python/uc3m_money/transfer_request.py
+++ src/main/python/uc3m_money/transfer_request.py
@@ -2,21 +2,19 @@
 import hashlib
 import json
 from datetime import datetime, timezone
-
-
+from uc3m_money.data.attr.iban_code import IbanCode
 class TransferRequest:
     """Class representing a transfer request"""
-
-    # pylint: disable=too-many-arguments
+    #pylint: disable=too-many-arguments
     def __init__(self,
                  from_iban: str,
                  transfer_type: str,
-                 to_iban: str,
-                 transfer_concept: str,
-                 transfer_date: str,
-                 transfer_amount: float):
-        self.__from_iban = from_iban
-        self.__to_iban = to_iban
+                 to_iban:str,
+                 transfer_concept:str,
+                 transfer_date:str,
+                 transfer_amount:float):
+        self.__from_iban = IbanCode(from_iban).value
+        self.__to_iban = IbanCode(to_iban).value
         self.__transfer_type = transfer_type
         self.__concept = transfer_concept
         self.__transfer_date = transfer_date
@@ -24,12 +22,8 @@ class TransferRequest:
         justnow = datetime.now(timezone.utc)
         self.__time_stamp = datetime.timestamp(justnow)
 
-    def signature_string(self) -> str:
-        """
-        Composes a string representation of the transfer data
-        to be hashed and used as the unique transfer code.
-        """
-        return f"Transfer:{json.dumps(self.__dict__)}"
+    def __str__(self):
+        return "Transfer:" + json.dumps(self.__dict__)
 
     def to_json(self):
         """returns the object information in json format"""
@@ -43,10 +37,9 @@ class TransferRequest:
             "time_stamp": self.__time_stamp,
             "transfer_code": self.transfer_code
         }
-
     @property
     def from_iban(self):
-        """Sender's IBAN."""
+        """Sender's iban"""
         return self.__from_iban
 
     @from_iban.setter
@@ -66,7 +59,6 @@ class TransferRequest:
     def transfer_type(self):
         """Property representing the type of transfer: REGULAR, INMEDIATE or URGENT """
         return self.__transfer_type
-
     @transfer_type.setter
     def transfer_type(self, value):
         self.__transfer_type = value
@@ -75,7 +67,6 @@ class TransferRequest:
     def transfer_amount(self):
         """Property respresenting the transfer amount"""
         return self.__transfer_amount
-
     @transfer_amount.setter
     def transfer_amount(self, value):
         self.__transfer_amount = value
@@ -84,26 +75,24 @@ class TransferRequest:
     def transfer_concept(self):
         """Property representing the transfer concept"""
         return self.__concept
-
     @transfer_concept.setter
     def transfer_concept(self, value):
         self.__concept = value
 
     @property
-    def transfer_date(self):
-        """Property representing the transfer's date in DD/MM/YYYY format"""
+    def transfer_date( self ):
+        """Property representing the transfer's date"""
         return self.__transfer_date
-
     @transfer_date.setter
-    def transfer_date(self, value):
+    def transfer_date( self, value ):
         self.__transfer_date = value
 
     @property
     def time_stamp(self):
-        """Read only property that returns the timestamp of the request"""
+        """Read-only property that returns the timestamp of the request"""
         return self.__time_stamp
 
     @property
     def transfer_code(self):
         """Returns the md5 signature (transfer code)"""
-        return hashlib.md5(self.signature_string().encode()).hexdigest()
+        return hashlib.md5(str(self).encode()).hexdigest()
